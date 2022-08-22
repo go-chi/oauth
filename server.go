acf8c6e8c39f240f8ee7a53cc9f11c5b3824ad34
chris <christhirst@gmail.com>
2022-08-22 23:17:09 +0200

    user return

--- server.go
+++ server.go
@@ -41,7 +41,7 @@ type CredentialsVerifier interface {
 	ValidateJwt(token string) (bool, error)
 
 	UserLookup(username, password, scope string) (map[string]string, error)
-	SessionGet(w http.ResponseWriter, r *http.Request, cookieID string) (bool, error)
+	SessionGet(w http.ResponseWriter, r *http.Request, cookieID string) (string, bool, error)
 	SessionSave(w http.ResponseWriter, r *http.Request, userID, cookieID string) (string, error)
 
 	StoreClientDelete(client []string) error
