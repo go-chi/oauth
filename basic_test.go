e25d8909b4fed944aae27073f5178e09eac2af81
chris <christhirst@gmail.com>
2022-09-20 23:05:11 +0200

    better tests

--- basic_test.go
+++ basic_test.go
@@ -2,50 +2,41 @@ package oauth
 
 import (
 	"encoding/base64"
-	"fmt"
 	"net/http"
-	"net/http/httptest"
+	"reflect"
 	"testing"
-	"time"
 )
 
-func TestA(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer slowServer.Close()
-	http.Get(slowServer.URL)
-
-}
-
 func TestGetBasicAuthentication(t *testing.T) {
-	username := "admin"
-	password := "password123456"
-	httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(username+":+"+password)))
-		rusername, rpassword, err := GetBasicAuthentication(r)
 
+	t.Run("get league", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "/token", nil)
+		req.Header.Set("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte("admin:password123456")))
+		want := []string{"admin", "password123456"}
+		got := make([]string, 2)
+		username, password, err := GetBasicAuthentication(req)
 		if err != nil {
 			t.Fatalf("Error %s", err.Error())
-		} else {
-			if rusername != username {
-				t.Fatalf("Wrong Username = %s", username)
-			}
-			if rpassword != password {
-				t.Fatalf("Wrong Username = %s", password)
-			}
 		}
-	}))
+		got[0] = username
+		got[1] = password
+
+		assertResponseBody(t, got, want)
+	})
+
 }
 
-// combine both test, create errors
+func assertResponseBody(t testing.TB, got, want []string) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
 func TestVoidBasicAuthentication(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/token", nil)
+
 	username, password, err := GetBasicAuthentication(req)
-	fmt.Println("ddd")
-	fmt.Println(username)
-	t.Error(err)
 	if err != nil {
 		t.Fatalf("Error %s", err.Error())
 	} else {
@@ -56,6 +47,7 @@ func TestVoidBasicAuthentication(t *testing.T) {
 			t.Fatalf("Wrong Username = %s", password)
 		}
 	}
+
 }
 
 func TestCheckBasicAuthentication(t *testing.T) {
