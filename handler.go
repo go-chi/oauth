f6fcfa3fe2e54f03e62980458cbbeb3f95679de6
chris <christhirst@gmail.com>
2022-10-14 22:40:22 +0200

    test

--- handler.go
+++ handler.go
@@ -25,7 +25,8 @@ func (bs *BearerServer) TokenEndpoint(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("code") != "" {
 		code = r.FormValue("code")
 	}
-
+	fmt.Println("qqqqqqqqqqqqqqqq")
+	fmt.Println(aud, grant_type, refresh_token, scope, code, redirect_uri, at)
 	resp, returncode, err := bs.GenerateIdTokenResponse("RS256", aud, grant_type, refresh_token, scope, code, redirect_uri, at, w, r)
 
 	if err != nil {
